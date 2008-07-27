73fe6171a310d1bb93a466f7370a7b61f52676bb
Jiang Shan <pasorobo@users.sourceforge.net>
2008-07-27 17:38:51 +0000

    [POCO::sandbox: Array ] Poco::Array update
    Details:
    
    1. Alignment update according to the POCO convension. (space -> tab, compact if-else )
    2. Add double guard in header file. (Although modern compilers can prevent the multiple-inclusion, IMHO)
    3. Add comment for public member's rationale.
    
    -------------------------------------------------------------
    
    TODO: 
    
    1. Real-world Samples.
    2. Add multithread testcases.
    2. Document.

--- Array/trunk/Foundation/include/Poco/Array.h
+++ Array/trunk/Foundation/include/Poco/Array.h
@@ -49,150 +49,152 @@
 #ifndef Foundation_Array_INCLUDED
 #define Foundation_Array_INCLUDED
 
+#ifndef Foundation_Exception_INCLUDED
 #include "Poco/Exception.h"
+#endif //  Foundation_Exception_INCLUDED
+
+#ifndef Foundation_Bugcheck_INCLUDED
 #include "Poco/Bugcheck.h"
+#endif //  Foundation_Bugcheck_INCLUDED
+
 #include <algorithm>
 
 namespace Poco {
 
 template<class T, std::size_t N>
 class Array 
-    /// STL container like C-style array replacement class. 
+	/// STL container like C-style array replacement class. 
 	/// 
 	/// This implementation is based on the idea of Nicolai Josuttis.
-    /// His original implementation can be found at http://www.josuttis.com/cppcode/array.html . 
+	/// His original implementation can be found at http://www.josuttis.com/cppcode/array.html . 
 {
-public:
-    T elems[N];    // fixed-size array of elements of type T
 
 public:
 
-    typedef T              value_type;
-    typedef T*             iterator;
-    typedef const T*       const_iterator;
-    typedef T&             reference;
-    typedef const T&       const_reference;
-    typedef std::size_t    size_type;
-    typedef std::ptrdiff_t difference_type;
+	typedef T				value_type;
+	typedef T*				iterator;
+	typedef const T*		const_iterator;
+	typedef T&				reference;
+	typedef const T&		const_reference;
+	typedef std::size_t		size_type;
+	typedef std::ptrdiff_t  difference_type;
 
-    iterator begin()
-    {
-        return elems;
-    }
+	iterator begin()
+	{
+		return elems;
+	}
 
-    const_iterator begin() const
-    {
-        return elems;
-    }
+	const_iterator begin() const
+	{
+		return elems;
+	}
 
-    iterator end()
-    { 
-        return elems+N;
-    }
+	iterator end()
+	{ 
+		return elems+N;
+	}
 
-    const_iterator end() const
-    {
-        return elems+N;
-    }
+	const_iterator end() const
+	{
+		return elems+N;
+	}
 
-    typedef std::reverse_iterator<iterator>         reverse_iterator;
-    typedef std::reverse_iterator<const_iterator>   const_reverse_iterator;
+	typedef std::reverse_iterator<iterator>			reverse_iterator;
+	typedef std::reverse_iterator<const_iterator>   const_reverse_iterator;
 
-    reverse_iterator rbegin()
-    {
-        return reverse_iterator(end());
-    }
+	reverse_iterator rbegin()
+	{
+		return reverse_iterator(end());
+	}
 
-    const_reverse_iterator rbegin() const
-    {
-        return const_reverse_iterator(end());
-    }
+	const_reverse_iterator rbegin() const
+	{
+		return const_reverse_iterator(end());
+	}
 
-    reverse_iterator rend()
-    { 
-        return reverse_iterator(begin());
-    }
+	reverse_iterator rend()
+	{ 
+		return reverse_iterator(begin());
+	}
 
-    const_reverse_iterator rend() const
-    {
-        return const_reverse_iterator(begin());
-    }
+	const_reverse_iterator rend() const
+	{
+		return const_reverse_iterator(begin());
+	}
 
-    reference operator[](size_type i) 
+	reference operator[](size_type i) 
 		/// element access without range check. If the index is not small than the given size, the behavior is undefined.
-    { 
-        poco_assert( i < N && "out of range" ); 
-        return elems[i];
-    }
+	{ 
+		poco_assert( i < N && "out of range" ); 
+		return elems[i];
+	}
 
-    const_reference operator[](size_type i) const 
+	const_reference operator[](size_type i) const 
 		/// element access without range check. If the index is not small than the given size, the behavior is undefined.
-    {     
-        poco_assert( i < N && "out of range" ); 
-        return elems[i]; 
-    }
+	{	 
+		poco_assert( i < N && "out of range" ); 
+		return elems[i]; 
+	}
 
-    reference at(size_type i)
+	reference at(size_type i)
 		/// element access with range check. Throws Poco::InvalidArgumentException if the index is over range.
-    { 
-        if(i>=size()){
-            throw Poco::InvalidArgumentException("Array::at() range check failed: index is over range");
-        }
-        return elems[i]; 
-    }
+	{ 
+		if(i>=size())
+			throw Poco::InvalidArgumentException("Array::at() range check failed: index is over range");
+		return elems[i]; 
+	}
 
-    const_reference at(size_type i) const
+	const_reference at(size_type i) const
 		/// element access with range check. Throws Poco::InvalidArgumentException if the index is over range.
-    {
-        if(i>=size()){
-            throw Poco::InvalidArgumentException("Array::at() range check failed: index is over range");
-        }
-        return elems[i]; 
-    }
+	{
+		if(i>=size())
+			throw Poco::InvalidArgumentException("Array::at() range check failed: index is over range");
+		return elems[i]; 
+	}
 
-    // front() and back()
-    reference front() 
-    { 
-        return elems[0]; 
-    }
+	// front() and back()
+	reference front() 
+	{ 
+		return elems[0]; 
+	}
 
-    const_reference front() const 
-    {
-        return elems[0];
-    }
+	const_reference front() const 
+	{
+		return elems[0];
+	}
 
-    reference back() 
-    { 
-        return elems[N-1]; 
-    }
+	reference back() 
+	{ 
+		return elems[N-1]; 
+	}
 
-    const_reference back() const 
-    { 
-        return elems[N-1]; 
-    }
+	const_reference back() const 
+	{ 
+		return elems[N-1]; 
+	}
 
-    static size_type size()
-    {
-        return N; 
-    }
+	static size_type size()
+	{
+		return N; 
+	}
 
-    static bool empty()
-    { 
-        return false; 
-    }
+	static bool empty()
+	{ 
+		return false; 
+	}
 
-    static size_type max_size()
-    { 
-        return N; 
-    }
+	static size_type max_size()
+	{ 
+		return N; 
+	}
 
-    enum { static_size = N };
+	enum { static_size = N };
 
-    void swap (Array<T,N>& y) {
-        std::swap_ranges(begin(),end(),y.begin());
-    }
+	void swap (Array<T,N>& y) {
+		std::swap_ranges(begin(),end(),y.begin());
+	}
 
-    const T* data() const
+	const T* data() const
 		/// direct access to data (read-only)
 	{ 
 		return elems; 
@@ -203,24 +205,29 @@ public:
 		return elems;
 	}
 
-    T* c_array(){ 
+	T* c_array(){ 
 		/// use array as C array (direct read/write access to data)
 		return elems;
 	}
 
-    template <typename Other>
-    Array<T,N>& operator= (const Array<Other,N>& rhs)
+	template <typename Other>
+	Array<T,N>& operator= (const Array<Other,N>& rhs)
 		/// assignment with type conversion 
 	{
-        std::copy(rhs.begin(),rhs.end(), begin());
-        return *this;
-    }
+		std::copy(rhs.begin(),rhs.end(), begin());
+		return *this;
+	}
 
-    void assign (const T& value)
+	void assign (const T& value)
 		/// assign one value to all elements
-    {
-        std::fill_n(begin(),size(),value);
-    }
+	{
+		std::fill_n(begin(),size(),value);
+	}
+
+public:
+
+	T elems[N];	// fixed-size array of elements of type T, public specifier used to make this class a aggregate.
+				   
 
 };
 
@@ -228,44 +235,44 @@ public:
 template<class T, std::size_t N>
 bool operator== (const Array<T,N>& x, const Array<T,N>& y) 
 {
-    return std::equal(x.begin(), x.end(), y.begin());
+	return std::equal(x.begin(), x.end(), y.begin());
 }
 
 template<class T, std::size_t N>
 bool operator< (const Array<T,N>& x, const Array<T,N>& y) 
 {
-    return std::lexicographical_compare(x.begin(),x.end(),y.begin(),y.end());
+	return std::lexicographical_compare(x.begin(),x.end(),y.begin(),y.end());
 }
 
 template<class T, std::size_t N>
 bool operator!= (const Array<T,N>& x, const Array<T,N>& y) 
 {
-    return !(x==y);
+	return !(x==y);
 }
 
 template<class T, std::size_t N>
 bool operator> (const Array<T,N>& x, const Array<T,N>& y) 
 {
-    return y<x;
+	return y<x;
 }
 
 template<class T, std::size_t N>
 bool operator<= (const Array<T,N>& x, const Array<T,N>& y) 
 {
-    return !(y<x);
+	return !(y<x);
 }
 
 template<class T, std::size_t N>
 bool operator>= (const Array<T,N>& x, const Array<T,N>& y) 
 {
-    return !(x<y);
+	return !(x<y);
 }
 
 // global swap()
 template<class T, std::size_t N>
 inline void swap (Array<T,N>& x, Array<T,N>& y) 
 {
-    x.swap(y);
+	x.swap(y);
 }
 
 }// namespace Poco
