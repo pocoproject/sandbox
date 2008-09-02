7ee855df745677fb5def15a2cce02e7b37dbe532
Jiang Shan <pasorobo@users.sourceforge.net>
2008-09-02 14:03:00 +0000

    [POCO::sandbox : Array ] Poco::Array branch for merge back to Poco::trunk
    Details: 
    
       * Add testsuite for Poco::Array to CoreTestSuite. Currently test cases include:
    
     	void testConstruction();
    	void testOperations();
    	void testContainer();
    	void testIterator();
    	void testAlgorithm();
    	void testMultiLevelArray();       
    
       * Premake based build scripts were add to build directory. Currently build scripts for
            
             vs2003
             vs2005
             vs2008
         
         were added. Note you need to build the necessary CppUnit/WinTestRunner lib/dll by yourself. 
         That is, also Foundation's testsuite uses these library and resource files, no dependency 
         specified in the scripts, just same as as Poco::trunk. 
    
       * Tested with VisualStudio 2008. 
    
       * Full Foundation library and testsuite were imported and tested for regression. 
    
       * Sample directory was removed to save space, currently no samples for this simple class. 
         The usage in testcases should be sufficient in my mind.

---
+++ Array/branches/Foundation/CppUnit/src/CppUnitException.cpp
@@ -0,0 +1,18 @@
+//
+// CppUnitException.cpp
+//
+// $Id: //poco/Main/CppUnit/src/CppUnitException.cpp#6 $
+//
+
+
+#include "CppUnit/CppUnitException.h"
+
+
+namespace CppUnit {
+
+
+const std::string CppUnitException::CPPUNIT_UNKNOWNFILENAME = "<unknown>";
+const int CppUnitException::CPPUNIT_UNKNOWNLINENUMBER = -1;
+
+
+} // namespace CppUnit
