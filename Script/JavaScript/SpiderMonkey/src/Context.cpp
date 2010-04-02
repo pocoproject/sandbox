e50e9c90355f467b62748156894ef4ca5918f278
Franky Braem <fbraem@users.sourceforge.net>
2010-04-02 13:24:43 +0000

    JSCLASS_GLOBAL_FLAGS is needed for global object

--- Script/JavaScript/SpiderMonkey/src/Context.cpp
+++ Script/JavaScript/SpiderMonkey/src/Context.cpp
@@ -61,7 +61,7 @@
 
 JSClass globalClass =
 {
-  "global", JSCLASS_HAS_PRIVATE,
+  "global", JSCLASS_HAS_PRIVATE | JSCLASS_GLOBAL_FLAGS,
   JS_PropertyStub,  JS_PropertyStub,
   JS_PropertyStub, JS_PropertyStub,
   JS_EnumerateStub, JS_ResolveStub,
